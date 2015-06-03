0d8758b5f18dacf735eb4f0a5ac0ee8840aa8cd0
Daniel Boston <programmerdan@gmail.com>
2015-06-03 00:23:55 -0400

    Starting to add minecraft scripts and such

---
+++ minecraft/get_and_start_vanilla.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.4/minecraft_server.1.8.4.jar -O /minecraft/minecraft_server.jar
+
+cd /minecraft
+
+java -Xms1G -Xmx1G -jar minecraft_server.jar nogui
+
+
