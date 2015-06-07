6e4ecfa7fe588f8b2f0e100ab1b54af0948a6411
Daniel Boston <programmerdan@gmail.com>
2015-06-07 02:07:06 -0400

    Mixing up control.sh

--- minecraft/control.sh
+++ minecraft/control.sh
@@ -5,11 +5,24 @@ echo but for now just do a dumb switch with Screen based controls
 
 cd /minecraft
 
-if [ $1 == 'start' ]
+if [[ "$1" == "start" ]]
 then
 	if [ -e '.mc_screen' ]
 	then
 		echo Minecraft is already in a screen session, joining to it now...
 		sleep 3
 		screen 
-	screen -d -m bash -c 'java -Xmx1500M -Xms500M -XX:MaxPermSize=256M -jar minecraft_server.jar nogui'
+	else
+		screen -d -S mc_screen -m bash -c 'java -Xmx1500M -Xms500M -XX:MaxPermSize=256M -jar minecraft_server.jar nogui'
+		touch .mc_screen
+	fi
+elif [[ "$1" == "stop" ]]
+then 
+	if [ -e '.mc_screen' ]
+	then
+		screen -S mc_screen -p 0 -X stuff 'stop'
+		rm .mc_screen
+	else
+		echo Minecraft seems to already be stopped?
+	fi
+fi
